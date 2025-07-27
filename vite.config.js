9388c6cdb0655410219f3f97acfd6abf1755b9dc
Dmytro <kravchukdima159@gmail.com>
2025-07-27 19:07:42 +0300

    Revert " Fix Netlify deployment issues"
    This reverts commit 07136ec0d4007fde812e4804b7de5a9eb18f3e70.

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,6 @@ export default {
       input: {
         main: 'index.html'
       }
-    },
-    // Copy additional files to dist
-    copyPublicDir: true
-  },
-  // Ensure all JS files are treated as assets
-  assetsInclude: ['**/*.js']
+    }
+  }
 } 
